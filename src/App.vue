8a211a1581617ed2870da6cdea9268d41f7f2eef
fork_web <leshadgrad@mail.ru>
2020-08-07 18:12:25 +0400

    start_project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="section_app">
+      <div class="container">
+        <ListNote msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListNote from './components/ListNote.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListNote
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" type="text/scss">
+  @import "~@/style/app.scss";
 </style>
---
+++ src/components/ListNote.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="notes">
+
+
+
+    <div class="list-notes">
+
+      <div class="notes-item">
+        <div class="notes-item__title">
+          <h3>Заголовок</h3>
+        </div>
+        <div class="notes-item__block">
+
+          <input type="checkbox" id="jack" value="1" v-model="checkedNoteItem">
+          <label for="jack">Выполнить первое задание</label>
+          <input type="checkbox" id="john" value="2" v-model="checkedNoteItem">
+          <label for="john">Выполнить второе задание</label>
+          <input type="checkbox" id="mike" value="5" v-model="checkedNoteItem">
+          <label for="mike">Выполнить пятое задание</label>
+          <br>
+          <span>Отмеченные заметки: {{ checkedNoteItem }}</span>
+          
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      checkedNoteItem: []
+    };
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" type="text/scss">
+  @import "~@/style/list_note.scss";
+</style>
